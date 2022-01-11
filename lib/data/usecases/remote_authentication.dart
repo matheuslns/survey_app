14c14bd0bdcc2d0530a94a8796f1b567ee0cb0af
Matheus Lima do Nascimento <matheus.l.nascimento.3@globalhitss.com.br>
2022-01-11 20:27:05 -0300

    ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -17,8 +17,14 @@ class RemoteAuthentication {
         method: 'post',
         body: RemoteAuthenticationParams.fromDomain(params).toJson(),
       );
-    } on HttpError {
-      throw DomainError.unexpected;
+    } on HttpError catch (error) {
+      switch (error) {
+        case HttpError.unauthorized:
+          throw DomainError.invalidCredentials;
+          break;
+        default:
+          throw DomainError.unexpected;
+      }
     }
   }
 }
