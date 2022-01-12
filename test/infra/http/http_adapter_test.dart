84bf876231ec64d38529471fedfe504d3bc3f3ad
Matheus Lima do Nascimento <matheus.l.nascimento.3@globalhitss.com.br>
2022-01-11 21:32:32 -0300

    ensure HttpAdapter calls post without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -21,10 +21,11 @@ class HttpAdapter {
       'accept': 'application/json'
     };
 
+    final jsonBody = body != null ? jsonEncode(body) : null;
     await client.post(
       url,
       headers: headers,
-      body: jsonEncode(body),
+      body: jsonBody,
     );
   }
 }
@@ -59,6 +60,15 @@ void main() {
           'accept': 'application/json'
         },
         body: '{"any_key":"any_value"}',
+      ));
+    });
+
+    test('Should call post without body', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(
+        any,
+        headers: anyNamed('headers'),
       ));
     });
   });
