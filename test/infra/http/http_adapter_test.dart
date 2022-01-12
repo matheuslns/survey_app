b161df9551726b45be10c6176ecef9dd548ff059
Matheus Lima do Nascimento <matheus.l.nascimento.3@globalhitss.com.br>
2022-01-11 21:24:08 -0300

    ensure HttpAdapter calls post with correct url and header

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -14,7 +14,12 @@ class HttpAdapter {
     @required String method,
     // Map body,
   }) async {
-    await client.post(url);
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+
+    await client.post(url, headers: headers);
   }
 }
 
@@ -29,7 +34,13 @@ void main() {
 
       await sut.request(url: url, method: 'post');
 
-      verify(client.post(url));
+      verify(client.post(
+        url,
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json'
+        },
+      ));
     });
   });
 }
