c1726fba99b9458d1f8e2f20209f38601296c783
Matheus Lima do Nascimento <matheus.l.nascimento.3@globalhitss.com.br>
2022-01-12 19:10:06 -0300

    Ensure disable button if form is invalid

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -167,4 +167,15 @@ void main() {
     final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
     expect(button.onPressed, isNotNull);
   });
+
+  testWidgets('Should disable button if form is valid',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    isFormValidController.add(false);
+    await tester.pump();
+
+    final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
+    expect(button.onPressed, null);
+  });
 }
