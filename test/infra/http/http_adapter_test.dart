be9bc1c3a4b2618cf707a6645d9381f90d4321e7
Matheus Lima do Nascimento <matheus.l.nascimento.3@globalhitss.com.br>
2022-01-11 22:20:36 -0300

    ensure HttpAdapter throws serverError if post throws

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -36,6 +36,10 @@ void main() {
       mockRequest().thenAnswer((_) async => Response(body, statusCode));
     }
 
+    void mockError() {
+      mockRequest().thenThrow(Exception());
+    }
+
     setUp(() {
       mockResponse(200);
     });
@@ -145,6 +149,14 @@ void main() {
       final future = sut.request(url: url, method: 'post');
 
       expect(future, throwsA(HttpError.notFound));
+    });
+
+    test('Should return ServerError if post throws', () async {
+      mockError();
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
     });
   });
 }
