a5b66fbb16faa590202c0cacf95df8006bd6c641
Matheus Lima do Nascimento <matheus.l.nascimento.3@globalhitss.com.br>
2022-01-11 21:29:29 -0300

    ensure HttpAdapter calls post with correct url, header, body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
@@ -12,14 +14,18 @@ class HttpAdapter {
   Future<void> request({
     @required String url,
     @required String method,
-    // Map body,
+    Map body,
   }) async {
     final headers = {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
 
-    await client.post(url, headers: headers);
+    await client.post(
+      url,
+      headers: headers,
+      body: jsonEncode(body),
+    );
   }
 }
 
@@ -38,7 +44,13 @@ void main() {
 
   group('post', () {
     test('Should call post with correct values', () async {
-      await sut.request(url: url, method: 'post');
+      await sut.request(
+        url: url,
+        method: 'post',
+        body: {
+          'any_key': 'any_value',
+        },
+      );
 
       verify(client.post(
         url,
@@ -46,6 +58,7 @@ void main() {
           'content-type': 'application/json',
           'accept': 'application/json'
         },
+        body: '{"any_key":"any_value"}',
       ));
     });
   });
