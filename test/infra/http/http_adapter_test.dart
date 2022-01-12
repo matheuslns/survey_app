51cef2524e72376767833a0dd1c7cc75b4d67c85
Matheus Lima do Nascimento <matheus.l.nascimento.3@globalhitss.com.br>
2022-01-11 21:44:34 -0300

    ensure HttpAdapter return null if post return 200 with no data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -30,7 +30,7 @@ class HttpAdapter implements HttpClient {
       body: jsonBody,
     );
 
-    return jsonDecode(response.body);
+    return response.body.isEmpty ? null : jsonDecode(response.body);
   }
 }
 
@@ -83,13 +83,22 @@ void main() {
       ));
     });
 
-    test('Should call return data if post returns 200', () async {
+    test('Should return data if post returns 200', () async {
       when(client.post(any, headers: anyNamed('headers')))
           .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
 
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, {"any_key": "any_value"});
+    });
+
+    test('Should return null if post returns 200 with no data', () async {
+      when(client.post(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => Response('', 200));
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
     });
   });
 }
