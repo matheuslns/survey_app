66862e7c01d54104895c8465f1e7a2af8f21b057
Matheus Lima do Nascimento <matheus.l.nascimento.3@globalhitss.com.br>
2022-01-13 17:46:12 -0300

    move production code to their own pages

---
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+
+import 'package:meta/meta.dart';
+
+import '../protocols/protocols.dart';
+import '../../ui/pages/pages.dart';
+
+class LoginState {
+  String emailError;
+}
+
+class StreamLoginPresenter implements LoginPresenter {
+  final Validation validation;
+  final _controller = StreamController<LoginState>.broadcast();
+  final _state = LoginState();
+
+  StreamLoginPresenter({@required this.validation});
+  @override
+  Stream<String> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError);
+
+  @override
+  // TODO: implement isFormValidStream
+  Stream<bool> get isFormValidStream => throw UnimplementedError();
+
+  @override
+  // TODO: implement isLoadingStream
+  Stream<bool> get isLoadingStream => throw UnimplementedError();
+
+  @override
+  // TODO: implement mainErrorStream
+  Stream<String> get mainErrorStream => throw UnimplementedError();
+
+  @override
+  // TODO: implement passwordErrorStream
+  Stream<String> get passwordErrorStream => throw UnimplementedError();
+
+  @override
+  void validateEmail(String email) {
+    _state.emailError = validation.validate(field: 'email', value: email);
+    _controller.add(_state);
+  }
+
+  @override
+  void validatePassword(String password) {
+    // TODO: implement validatePassword
+  }
+
+  @override
+  Future<void> auth() {
+    // TODO: implement auth
+    throw UnimplementedError();
+  }
+
+  @override
+  void dispose() {
+    _controller.close();
+  }
+}
--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,68 +1,9 @@
-import 'dart:async';
-
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:meta/meta.dart';
 
-import 'package:survey_app/ui/pages/pages.dart';
-
-class LoginState {
-  String emailError;
-}
-
-class StreamLoginPresenter implements LoginPresenter {
-  final Validation validation;
-  final _controller = StreamController<LoginState>.broadcast();
-  final _state = LoginState();
-
-  StreamLoginPresenter({@required this.validation});
-  @override
-  Stream<String> get emailErrorStream =>
-      _controller.stream.map((state) => state.emailError);
-
-  @override
-  // TODO: implement isFormValidStream
-  Stream<bool> get isFormValidStream => throw UnimplementedError();
-
-  @override
-  // TODO: implement isLoadingStream
-  Stream<bool> get isLoadingStream => throw UnimplementedError();
-
-  @override
-  // TODO: implement mainErrorStream
-  Stream<String> get mainErrorStream => throw UnimplementedError();
-
-  @override
-  // TODO: implement passwordErrorStream
-  Stream<String> get passwordErrorStream => throw UnimplementedError();
-
-  @override
-  void validateEmail(String email) {
-    _state.emailError = validation.validate(field: 'email', value: email);
-    _controller.add(_state);
-  }
-
-  @override
-  void validatePassword(String password) {
-    // TODO: implement validatePassword
-  }
-
-  @override
-  Future<void> auth() {
-    // TODO: implement auth
-    throw UnimplementedError();
-  }
-
-  @override
-  void dispose() {
-    _controller.close();
-  }
-}
-
-abstract class Validation {
-  String validate({@required String field, @required String value});
-}
+import 'package:survey_app/presentation/presenters/presenters.dart';
+import 'package:survey_app/presentation/protocols/protocols.dart';
 
 class ValidationSpy extends Mock implements Validation {}
 
