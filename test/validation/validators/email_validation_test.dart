309c817161b871b41f87caae21216c266a4b2134
Matheus Lima do Nascimento <matheus.l.nascimento.3@globalhitss.com.br>
2022-01-14 19:07:52 -0300

    ensure email validation return correct values when email is valid or not

---
+++ test/validation/validators/email_validation_test.dart
@@ -0,0 +1,28 @@
+import 'package:faker/faker.dart';
+import 'package:test/test.dart';
+
+import 'package:survey_app/validation/validators/validators.dart';
+
+void main() {
+  EmailValidation sut;
+
+  setUp(() {
+    sut = EmailValidation('any_field');
+  });
+
+  test('Should return null if email is empty', () {
+    expect(sut.validate(''), null);
+  });
+
+  test('Should return null if email is null', () {
+    expect(sut.validate(null), null);
+  });
+
+  test('Should return Email inválido if email is invalid', () {
+    expect(sut.validate('invalid_email'), 'Email inválido');
+  });
+
+  test('Should return null if email is valid', () {
+    expect(sut.validate(faker.internet.email()), null);
+  });
+}
