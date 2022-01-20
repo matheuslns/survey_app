f29f63dd4f3aeed518a8ca840f56b86a9af304c3
Matheus Lima do Nascimento <matheus.l.nascimento.3@globalhitss.com.br>
2022-01-20 18:55:19 -0300

    ensure LocalLoadCurrentAccount call FetchSecureCacheStorage with correct value

---
+++ test/data/usecases/load_ccurent_account/local_load_current_account_test.dart
@@ -0,0 +1,37 @@
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+class LocalLoadCurrentAccount {
+  final FetchSecureCacheStorage fetchSecureCacheStorage;
+
+  LocalLoadCurrentAccount({@required this.fetchSecureCacheStorage});
+
+  Future<void> load() async {
+    fetchSecureCacheStorage.fetchSecure('token');
+  }
+}
+
+abstract class FetchSecureCacheStorage {
+  Future<void> fetchSecure(String key);
+}
+
+class FetchSecureCacheStorageSpy extends Mock
+    implements FetchSecureCacheStorage {}
+
+void main() {
+  LocalLoadCurrentAccount sut;
+  FetchSecureCacheStorage fetchSecureCacheStorage;
+
+  setUp(() {
+    fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
+    sut = LocalLoadCurrentAccount(
+      fetchSecureCacheStorage: fetchSecureCacheStorage,
+    );
+  });
+  test('Should call FetchSecureCacheStorage with correct value', () async {
+    await sut.load();
+
+    verify(fetchSecureCacheStorage.fetchSecure('token'));
+  });
+}
