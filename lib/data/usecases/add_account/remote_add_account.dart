471f1458cd8da84c5e414c172d18f63c1b152b7a
Matheus Lima do Nascimento <matheus.l.nascimento.3@globalhitss.com.br>
2022-01-24 18:45:45 -0300

    add usecade remote add account

---
+++ lib/data/usecases/add_account/remote_add_account.dart
@@ -0,0 +1,63 @@
+import 'package:meta/meta.dart';
+
+import '../../models/models.dart';
+import '../../../domain/entities/entities.dart';
+import '../../../domain/helpers/helpers.dart';
+import '../../../domain/usecases/usecases.dart';
+import '../../http/http.dart';
+
+class RemoteAddAccount implements AddAccount {
+  final HttpClient httpClient;
+  final String url;
+
+  RemoteAddAccount({@required this.httpClient, @required this.url});
+
+  Future<AccountEntity> add(AddAccountParams params) async {
+    try {
+      final response = await httpClient.request(
+        url: url,
+        method: 'post',
+        body: RemoteAddAccountParams.fromDomain(params).toJson(),
+      );
+
+      return RemoteAccountModel.fromJson(response).toEntity();
+    } on HttpError catch (error) {
+      switch (error) {
+        case HttpError.unauthorized:
+          throw DomainError.invalidCredentials;
+          break;
+        default:
+          throw DomainError.unexpected;
+      }
+    }
+  }
+}
+
+class RemoteAddAccountParams {
+  final String email;
+  final String name;
+  final String password;
+  final String passwordConfirmation;
+
+  RemoteAddAccountParams({
+    @required this.email,
+    @required this.name,
+    @required this.password,
+    @required this.passwordConfirmation,
+  });
+
+  factory RemoteAddAccountParams.fromDomain(AddAccountParams entity) =>
+      RemoteAddAccountParams(
+        email: entity.email,
+        name: entity.name,
+        password: entity.password,
+        passwordConfirmation: entity.passwordConfirmation,
+      );
+
+  Map toJson() => {
+        'email': email,
+        'name': name,
+        'password': password,
+        'passwordConfirmation': passwordConfirmation,
+      };
+}
---
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -0,0 +1,118 @@
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:survey_app/data/usecases/add_account/remote_add_account.dart';
+import 'package:survey_app/domain/helpers/helpers.dart';
+import 'package:test/test.dart';
+
+import 'package:survey_app/data/http/http.dart';
+import 'package:survey_app/domain/usecases/usecases.dart';
+
+class HttpClientSpy extends Mock implements HttpClient {}
+
+void main() {
+  RemoteAddAccount sut;
+  HttpClientSpy httpClient;
+  String url;
+  AddAccountParams params;
+
+  Map mockValidData() => {
+        'accessToken': faker.guid.guid(),
+        'name': faker.person.name(),
+      };
+
+  PostExpectation mockRequest() => when(httpClient.request(
+        url: anyNamed('url'),
+        method: anyNamed('method'),
+        body: anyNamed('body'),
+      ));
+
+  void mockHttpData(Map data) {
+    mockRequest().thenAnswer((_) async => data);
+  }
+
+  void mockHttpError(HttpError error) {
+    mockRequest().thenThrow(error);
+  }
+
+  setUp(() {
+    httpClient = HttpClientSpy();
+    url = faker.internet.httpUrl();
+    sut = RemoteAddAccount(httpClient: httpClient, url: url);
+
+    params = AddAccountParams(
+      email: faker.internet.email(),
+      name: faker.person.name(),
+      password: faker.internet.password(),
+      passwordConfirmation: faker.internet.password(),
+    );
+
+    mockHttpData(mockValidData());
+  });
+
+  test('Should call HttpClient with correct values', () async {
+    await sut.add(params);
+
+    verify(httpClient.request(
+      url: url,
+      method: 'post',
+      body: {
+        'email': params.email,
+        'name': params.name,
+        'password': params.password,
+        'passwordConfirmation': params.passwordConfirmation,
+      },
+    ));
+  });
+
+  test('Should throw UnexpactedError if HttpClient returns 400', () async {
+    mockHttpError(HttpError.badRequest);
+
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpactedError if HttpClient returns 404', () async {
+    mockHttpError(HttpError.notFound);
+
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpactedError if HttpClient returns 500', () async {
+    mockHttpError(HttpError.serverError);
+
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw InvalidCredentialsError if HttpClient returns 401',
+      () async {
+    mockHttpError(HttpError.unauthorized);
+
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.invalidCredentials));
+  });
+
+  test('Should return an Account if HttpClient returns 200', () async {
+    final validData = mockValidData();
+    mockHttpData(validData);
+
+    final account = await sut.add(params);
+
+    expect(account.token, validData['accessToken']);
+  });
+
+  test(
+      'Should return throw UnexpactedError if HttpClient returns 200 with invalid data',
+      () async {
+    mockHttpData({'invalid_key': 'invalid_value'});
+
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+}
